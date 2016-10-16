bcde2a3c7f30845fc963359bfb6e2d737de0d0ed
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-16 12:30:56 -0600

    added functions.js and a function to format javascript date()

--- public_html/index.php
+++ public_html/index.php
@@ -2,67 +2,117 @@
 /**
  * Created by PhpStorm.
  * User: Matthew
- * Date: 10/11/2016
- * Time: 10:18 AM
+ * Date: 10/12/2016
+ * Time: 11:00 AM
  */
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/main.css" type="text/css" />
-		<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/8bit-wonder" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="js/functions.js"></script>
+		<script>
+
+			$( document ).ready(function() {
+				$("#aboutMe").click(function() {
+					$("#thing").slideToggle(function() {
+						// Animation complete.
+					});
+				});
+			});
+
+
+		</script>
+
+		<!--made using favicon-generator.com-->
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="img/favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+		<link rel="manifest" href="img/favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+		<title>Data Design for Wanikani Radicals</title>
 	</head>
 	<body>
 		<!-- the header -->
 		<header>
 			<div class="header-main-text">
-				<h1>Hello World</h1>
+				<h1><script>dateFormat()</script></h1>
 			</div>
 			<div class="header-sub-text">
-				<h4>Welcome to my halfassed website</h4>
+				<h4>For Wanikani profile and radicals</h4>
 			</div>
 		</header>
 		<!--main content-->
 		<main>
-			<!--begin section 1-->
+			<!--begin section 1 (persona section)-->
 			<section>
-				<!--samuel l ipsum-->
-				<h2>Some random text</h2>
-				<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p>
-				<div class="image-holder">
-					<img src="img/party.jpg" alt="You party like shit">
+				<h1 id="aboutMe">About me</h1>
+				<div id="thing">
+					<p>yo!</p>
 				</div>
 			</section>
 			<section>
-				<h2>Stuff I like</h2>
-				<ul>
-					<li>food</li>
-					<li>water</li>
-					<li>etc</li>
-				</ul>
+				<h2>Use Case and Interaction Flow</h2>
+				<details>
+					<summary></summary>
+					<h3>Use Case</h3>
+					<p>John Meerschwein just got home from work. He's exhausted and slightly annoyed from working with children all evening, he just wants to relax. However, John is also annoyed that he got lost on his way home again due to his inability to read Kanji. John decides enough is enough, he needs to learn Kanji. Being about as motivated to do anything as John ever has been in his whole life, he searches for "Learn kanji easily" on his PC. He finds wanikani, which starts creates and manages flash cards for you, starting with kanji radicals.</p>
+					<h3>Interaction Flow</h3>
+					<ol>
+						<li>John creates an account and logs in on his favorite web browser on his PC</li>
+						<li>John clicks on the "lessons" button and reads through the new items he needs to memorize</li>
+						<li>John takes the quiz for the items he just read through</li>
+						<li>John logs out for the night</li>
+						<li>The next day after work, John logs back in an reviews the items he learned yesterday</li>
+						<li>John logs out again for the night</li>
+					</ol>
+				</details>
 			</section>
 			<section>
-				<table>
-					<tr>
-						<th>Column One</th>
-						<th>Column Two</th>
-					</tr>
-					<tr>
-						<td>Cat</td>
-						<td>Dog</td>
-					</tr>
-					<tr>
-						<td>Car</td>
-						<td>Bike</td>
-					</tr>
-					<tr>
-						<td>Starwars</td>
-						<td>Wine</td>
-					</tr>
-				</table>
+				<h2>Entities and Attributes</h2>
+				<details>
+					<summary></summary>
+					<ul>Radicals *Strong*
+						<li>radicalId (PRIMARY_KEY AUTO_INCREMENT)</li>
+						<li>radical</li>
+						<li>radicalLevel</li>
+						<li>radicalCorrectThreshold</li>
+					</ul>
+					<ul>Profile *Strong*
+						<li>userId (PRIMARY_KEY AUTO_INCREMENT)</li>
+						<li>userName</li>
+						<li>userEmail</li>
+						<li>userLevel</li>
+					</ul>
+					<ul>Attempts *Weak*
+						<li>attemptsUserId (FOREIGN_KEY)</li>
+						<li>attemptsRadicalId (FOREIGN_KEY)</li>
+						<li>attemptsIsCorrect</li>
+						<li>attemptsTimeTested</li>
+					</ul>
+					<br>
+					<div class="image-holder">
+						<img src="img/wanikaniErd.svg" alt="Wanikani ERD">
+					</div>
+				</details>
 			</section>
 		</main>
+
 	</body>
 </html>
---
+++ public_html/js/functions.js
@@ -0,0 +1,112 @@
+//dateFormat function (formats date)
+function dateFormat() {
+	//new js date
+	var date = new Date();
+	//changing the output the date to a string
+	var dateString = date.toString();
+	//changing the string into an array
+	var myDate = dateString.split(" ")
+	//pulling the day of the week from the array and sending it to lowercase
+	var protoDay = myDate[0].toLowerCase();
+	//var for day of the week
+	var day;
+	//this switch statement changes the day of the week from a short form to a long form
+	switch(protoDay) {
+		case "sun":
+			var day = "Sunday"
+			break;
+		case "mon":
+			var day = "Monday"
+			break;
+		case "tue":
+			var day = "Tuesday"
+			break;
+		case "wed":
+			var day = "Wednesday"
+			break;
+		case "thu":
+			var day = "Thursday"
+			break;
+		case "fri":
+			var day = "Friday"
+			break;
+		case "sat":
+			var day = "Saturday"
+			break;
+	}
+	//pulls the month from the array
+	var protoMonth = myDate[1];
+	//changes the short form of the month to the long form
+	switch(protoMonth.toLowerCase()) {
+		case "jan":
+			var month = "January"
+			break;
+		case "feb":
+			var month = "February"
+			break;
+		case "mar":
+			var month = "March"
+			break;
+		case "apr":
+			var month = "April"
+			break;
+		case "may":
+			var month = "May"
+			break;
+		case "jun":
+			var month = "June"
+			break;
+		case "jul":
+			var month = "July"
+			break;
+		case "aug":
+			var month = "August"
+			break;
+		case "sep":
+			var month = "September"
+			break;
+		case "oct":
+			var month = "October"
+			break;
+		case "nov":
+			var month = "November"
+			break;
+		case "dec":
+			var month = "December"
+			break;
+	}
+	//pulls the day of the month from the array
+	var dayOfMonth = myDate[2];
+	//if the last character in the  date is a 1
+	if(dayOfMonth.slice(-1) == 1) {
+		//if the date is  not the 11th
+		if(dayOfMonth != 11) {
+			var theDate = dayOfMonth + "st";
+		}
+		else {
+			var theDate = dayOfMonth + "th";
+		}
+	}
+	//if the last character in the date is a 2
+	else if(dayOfMonth.slice(-1) == 2) {
+		var theDate = dayOfMonth + "nd";
+	}
+	else {
+		//if the last character in the date is a 3
+		if(dayOfMonth.slice(-1) == 3) {
+			var theDate = dayOfMonth + "rd";
+		}
+		else {
+			var theDate = dayOfMonth + "th";
+		}
+	}
+	//if the date is single digit (automatically formatted with a leading 0)
+	if(theDate.charAt(0) == 0) {
+		//returns only the last digit and the date suffix
+		var theDate = theDate.slice(-3);
+	}
+	//pulls the year from the  array
+	var year = myDate[3];
+	//concatinates the newley formated date
+	document.write(day + ", " + month +" "+ theDate +", "+ year);
+};
